daf96a0535b4545d72cac173fa70a057b69d3d2f
Joshua Smith <joshua.aaron.smith17@gmail.com>
2016-10-19 14:07:40 -0400

    added guess functions

--- src/components/Guess.js
+++ src/components/Guess.js
@@ -1,7 +1,75 @@
 const h = require('react-hyperscript')
+const React = require('react')
 
-module.exports = _ =>
-  h('h1', 'TODO: Guess App')
+const generatedAnswer = function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+const generatedGuess = function getRandomIntInclusive (min, max) {
+  min = Math.ceil(min)
+  max = Math.floor(max)
+  return Math.floor(Math.random() * (max - min + 1)) + min
+}
+
+const GuessNumber = React.createClass({
+  getInitialState: function () {
+    return { number: generatedAnswer(1,10).toString(),
+      guess: '',
+      comment: '',
+      answer: '',
+      computerComment: '',
+      computerNumber: generatedGuess(1,10).toString()}
+  },
+  setGuess: function (e) {
+     this.setState({ guess: e.target.value })
+  },
+  setAnswer: function (e) {
+    this.setState({ answer: e.target.value })
+  },
+  onGuess: function (event) {
+    if (this.state.guess === this.state.number) {
+      return this.setState({ comment: 'Correct!' })
+    } else if (this.state.guess > this.state.number) {
+      return this.setState({ comment: 'Lower' })
+    } else if (this.state.guess < this.state.number) {
+      return this.setState({ comment: 'Higher' })
+    }
+
+    console.log(this.state.guess)
+    console.log(this.state.number)
+  },
+  onAnswer: function (event) {
+    if (this.state.answer === this.state.computerNumber) {
+      return this.setState({ computerComment: 'Correct!' })
+    } else if (this.state.answer !== this.state.computerNumber) {
+      return this.setState({ computerComment: 'You tricked the computer!' })
+    }
+  },
+  render () {
+    return (
+      h('div', [
+        h('h1', 'Guess a Number Between 1-10'),
+        h('input', {
+          onChange: this.setGuess,
+          value: this.state.guess,
+          placeholder: 'Your Number Goes Here'
+        }),
+        h('button', { onClick: this.onGuess }, 'Guess'),
+        h('div', this.state.comment),
+        h('h1', 'Give the computer a number to guess!'),
+        h('input', {
+          onChange: this.setAnswer,
+          value: this.state.answer,
+          placeholder: 'Try and trick the computer!'
+        }),
+        h('button', { onClick: this.onAnswer }, 'Ask'),
+        h('div', this.state.computerComment)
+      ])
+    )
+  }
+})
+module.exports = GuessNumber
 
 // have the computer randomly select number 1 - 10
 // and have the user try to guess the number
--- src/components/Home.js
+++ src/components/Home.js
@@ -5,10 +5,10 @@ module.exports = _ =>
   h('div', [
     h('h2', 'Apps'),
     h('ul', [
-      h('li', [ h(Link, { to: '/'}, 'Home')]),
-      h('li', [ h(Link, { to: '/hello'}, 'Say Hello')]),
-      h('li', [ h(Link, { to: '/guess'}, 'Guess A Number')]),
-      h('li', [ h(Link, { to: '/eightball'}, 'Magic Eight Ball')]),
-      h('li', [ h(Link, { to: '/calculator'}, 'Calculator')])
+      h('li', [h(Link, { to: '/' }, 'Home')]),
+      h('li', [h(Link, { to: '/hello' }, 'Say Hello')]),
+      h('li', [h(Link, { to: '/guess' }, 'Guess A Number')]),
+      h('li', [h(Link, { to: '/eightball' }, 'Magic Eight Ball')]),
+      h('li', [h(Link, { to: '/calculator' }, 'Calculator')])
     ])
   ])
